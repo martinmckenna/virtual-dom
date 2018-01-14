e7e858f719cb9c5cc60e807717acd0c78528a373
Marty <mmckenna.phila@gmail.com>
2018-01-14 13:22:48 -0500

    add to wireframe
    add links, styles, and dev icons

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 const date = new Date();
+const yearOfBirth = 1994
 
 document
     .getElementById('year')
     .innerText = date.getFullYear();
+
+let age = date.getFullYear() - yearOfBirth;
+
+if (date.getMonth() < 9) {
+    age = age - 1;
+}
+
+document.getElementById('age').innerText = age;
