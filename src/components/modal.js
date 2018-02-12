6f860ee889c6649c87106497c9a84abb467c341e
Marty <mmckenna.phila@gmail.com>
2018-02-11 21:47:58 -0500

    remove modals, change icons

--- src/components/modal.js
+++
@@ -1,9 +0,0 @@
-export const modalProperties = {
-    footer: true,
-    stickyFooter: true,
-    closeMethods: [
-        'overlay', 'button', 'escape'
-    ],
-    closeLabel: 'Close',
-    cssClass: ['modal']
-};
