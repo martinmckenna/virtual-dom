9417ce9cc081e7c790cae6accaefd4e6d0bb0967
Marty <mmckenna.phila@gmail.com>
2018-03-29 16:36:23 -0400

    add form validation and projects

--- src/components/contact.js
+++ src/components/contact.js
@@ -10,15 +10,28 @@ const submitForm = e => {
     formToJSON[key] = value;
   }
 
-  return fetch('/email.php', {
-    body: JSON.stringify(formToJSON),
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    method: 'POST'
-  }).then(response => {
-    console.log(response);
-  });
+  let quizAnswer = document.getElementById('quiz');
+
+  if (quizAnswer.value == 14) {
+    return fetch('/email.php', {
+      body: JSON.stringify(formToJSON),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: 'POST'
+    }).then(response => {
+      if (!!response.ok) {
+        alert('Message sent successfully!');
+      } else {
+        alert('There was an issue sending your message. Please try again!');
+        form.reset();
+        quizAnswer.style.borderColor = 'grey';
+      }
+    });
+  } else {
+    alert('Please answer the quiz answer correctly!');
+    quizAnswer.style.borderColor = 'red';
+  }
 };
 
 const form = document.getElementById('contact-form');
