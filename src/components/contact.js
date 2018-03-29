1a067c7aa54e05b3cb417dcc4d66bda58f8a6d63
Marty <mmckenna.phila@gmail.com>
2018-03-29 14:21:25 -0400

    change contact js

--- src/components/contact.js
+++ src/components/contact.js
@@ -2,21 +2,20 @@ const submitForm = e => {
   e.preventDefault();
 
   let formData = new FormData(form);
-  formData.append("name", document.getElementById("input-name").value);
-  formData.append("email", document.getElementById("input-email").value);
-  formData.append("desc", document.getElementById("input-desc").value);
+  formData.append('name', document.getElementById('input-name').value);
+  formData.append('email', document.getElementById('input-email').value);
+  formData.append('desc', document.getElementById('input-desc').value);
 
-  return fetch("/email.php", {
+  return fetch('/email.php', {
     body: formData,
     headers: {
-      Accept: "application/json, application/xml, text/plain, text/html, *.*",
-      "Content-Type": "application/x-www-form-urlencoded; charset=utf-8"
+      'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
     },
-    method: "POST"
+    method: 'POST'
   }).then(response => {
     console.log(response);
   });
 };
 
-const form = document.getElementById("contact-form");
-form.addEventListener("submit", e => submitForm(e));
+const form = document.getElementById('contact-form');
+form.addEventListener('submit', e => submitForm(e));
