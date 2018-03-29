21cb646f8cd52c801cdb39fadd34aae340d199a7
Marty <mmckenna.phila@gmail.com>
2018-03-29 15:41:28 -0400

    send mail with json data from frontend

--- email.php
+++ email.php
@@ -7,4 +7,14 @@ error_reporting(E_ALL);
 $content = trim(file_get_contents("php://input"));
 $content = json_decode($content);
 
+$email = $content->email;
+$name = $content->name;
+$message = $content->desc;
+
+mail('mmckenna.phila@gmail.com', 'ATMARTY: From ' . $email, 'Name: ' . $name . '
+
+' . 'Email ' . $email . '
+
+' . $message);
+
 file_put_contents('logs.txt', print_r($content, true)."\n\n", FILE_APPEND);
