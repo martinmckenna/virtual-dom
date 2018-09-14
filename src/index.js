99d1c68d1c7c7aa2e418b4226fd3922116533b8f
Marty <mmckenna.phila@gmail.com>
2018-09-14 16:29:32 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-import './styles/styles.css'
+import './styles/styles.css';
+import './bloop';
+
+const Hello = () => {
+  return Bloop.createElement('div', null, 'Hello World')
+}
+
+const helloWorld = Bloop.createElement(Hello, null, `Hello World`);
+BloopDOM.render(helloWorld, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const ExtractTextPlugin = require("extract-text-webpack-plugin"); // compile css
+const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // compile css
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin'); // minimize css
 const HtmlWebpackPlugin = require('html-webpack-plugin'); // make html file from template
 const FileManagerPlugin = require('filemanager-webpack-plugin');
@@ -7,72 +7,76 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
-    entry: {
-        js: './src/index.js'
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    devServer: {
-        contentBase: './dist',
-        watchContentBase: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }, {
-                test: /\.css$/,
-                include: [
-                    path.resolve(__dirname, "./src/styles/"),
-                    "/node_modules/"
-                ],
-                use: ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: [
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                minimize: true
-                            }
-                        }
-                    ]
-                })
-            }
-        ]
-    },
-    plugins: [
-        new ExtractTextPlugin("styles.min.css"),
-        new OptimizeCssAssetsPlugin({
-            assetNameRegExp: /\.optimize\.css$/g,
-            cssProcessor: require('cssnano'),
-            cssProcessorOptions: {
-                discardComments: {
-                    removeAll: true
-                }
-            },
-            canPrint: true
-        }),
-        new UglifyJSPlugin(),
-        new HtmlWebpackPlugin({ title: 'AtMarty - Home', template: './dist/templates/index.html' }),
-        new FileManagerPlugin({
-            onEnd: {
-                move: [
-                    {
-                        source: 'dist/index.html',
-                        destination: './index.html'
-                    }, {
-                        source: 'dist/bundle.js',
-                        destination: './bundle.js'
-                    }, {
-                        source: 'dist/styles.min.css',
-                        destination: './styles.min.css'
-                    }
-                ]
-            }
-        })
-    ]
+	entry: {
+		js: './src/index.js'
+	},
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	devServer: {
+		contentBase: './dist',
+		watchContentBase: true
+	},
+	mode: 'development',
+	devtool: 'inline-source-map',
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: "babel-loader"
+			}, {
+				test: /\.css$/,
+				include: [
+					path.resolve(__dirname, "./src/styles/"),
+					"/node_modules/"
+				],
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+					},
+					"css-loader"
+				]
+			}
+		]
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			// Options similar to the same options in webpackOptions.output
+			// both options are optional
+			filename: "[name].css",
+			chunkFilename: "[id].css"
+		}),
+		new OptimizeCssAssetsPlugin({
+			assetNameRegExp: /\.optimize\.css$/g,
+			cssProcessor: require('cssnano'),
+			cssProcessorOptions: {
+				discardComments: {
+					removeAll: true
+				}
+			},
+			canPrint: true
+		}),
+		new UglifyJSPlugin({
+			sourceMap: true,
+		}),
+		new HtmlWebpackPlugin({ title: 'AtMarty - Home', template: './dist/templates/index.html' }),
+		new FileManagerPlugin({
+			onEnd: {
+				move: [
+					{
+						source: 'dist/index.html',
+						destination: './index.html'
+					}, {
+						source: 'dist/bundle.js',
+						destination: './bundle.js'
+					}, {
+						source: 'dist/styles.min.css',
+						destination: './styles.min.css'
+					}
+				]
+			}
+		})
+	]
 };
